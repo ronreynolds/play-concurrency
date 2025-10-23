c67282b3b96aa7fb54d7f00fc1eb52b15c83d4ba
Ron Reynolds <ron@ronreynolds.com>
2025-10-23 15:08:08 -0700

    chore(main): moved FQCN to import for tidier code

--- src/main/java/com/ronreynolds/tools/Main.java
+++ src/main/java/com/ronreynolds/tools/Main.java
@@ -1,10 +1,11 @@
 package com.ronreynolds.tools;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        try (ExecutorService executor = java.util.concurrent.Executors.newFixedThreadPool(4)) {
+        try (ExecutorService executor = Executors.newFixedThreadPool(4)) {
             for (int i = 0; i < 10; i++) {
                 executor.execute(() -> System.out.println("Hello, World!"));
             }
